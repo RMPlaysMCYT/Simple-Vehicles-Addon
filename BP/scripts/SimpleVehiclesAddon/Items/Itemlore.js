32c3c7eb1a25569362ce2e839afce722a9b1736b
RMPlaysMC YT <76448296+RMPlaysMCYT@users.noreply.github.com>
2026-06-08 04:45:30 +0800

    fixing the Cadillac DTS 2010

--- BP/scripts/SimpleVehiclesAddon/Items/Itemlore.js
+++ BP/scripts/SimpleVehiclesAddon/Items/Itemlore.js
@@ -9,71 +9,6 @@
 import { world, system } from "@minecraft/server";
 import { VEHICLES_EGGS } from "./itemEggs";
 
-/*const lore_items = {
-    "simple_vehicles:jump1_item": [
-        "Controls:",
-        "   Right Click to Jump"
-    ],
-    "simple_vehicles:jump2_item": [
-        "Controls:",
-        "   Right Click to Jump"
-    ],
-    "simple_vehicles:jump3_item": [
-        "Controls:",
-        "   Right Click to Jump"
-    ],
-    "simple_vehicles:honk_item": [
-        "Controls:",
-        "   Mobile: Press Cross or Long-Press to Honk",
-        "   PC: Right Click to Honk",
-        "   Gamepad: Press R2, RT to Honk"
-    ],
-    "simple_vehicles:beetrpack": [
-        "Controls:",
-        "   Right Click to Add to your Tractor"
-    ],
-    "simple_vehicles:wheatpack": [
-        'Controls:',
-        '   Right Click to Add to your Tractor'
-    ],
-    "simple_vehicles:mixer_placer": [
-        "Controls:",
-        "   Right Click to Place some stuff"
-    ],
-    "simple_vehicles:asphalt_paving_placer": [
-        "Controls:",
-        "   Right Click to Place some stuff"
-    ],
-    "simple_vehicles:bridge_building_placer": [
-        "Controls:",
-        "   Right Click to Place some stuff"
-    ],
-    "simple_vehicles:jerry_can": [
-        "Controls: ",
-        "   Mobile: Interact or Long-Press to Fuel A Vehicle",
-        "   PC: Right Click to Fuel A Vehicle",
-        "   Gamepad: Press R2, RT to Fuel A Vehicle"
-    ],
-    'simple_vehicles:air_jerry_can': [
-        'Controls: ',
-        '   Mobile: Interact or Long-Press to Fuel A Vehicle',
-        '   PC: Right Click to Fuel A Vehicle',
-        '   Gamepad: Press R2, RT to Fuel A Vehicle'
-    ],
-    'simple_vehicles:gas_nozzle_air': [
-        'Controls: ',
-        '   Mobile: Interact or Long-Press to Fuel A Vehicle',
-        '   PC: Right Click to Fuel A Vehicle',
-        '   Gamepad: Press R2, RT to Fuel A Vehicle'
-    ],
-    'simple_vehicles:gas_nozzle': [
-        "Controls: ",
-        "   Mobile: Interact or Long-Press to Fuel A Vehicle",
-        "   PC: Right Click to Fuel A Vehicle",
-        "   Gamepad: Press R2, RT to Fuel A Vehicle"
-    ]
-}*/
-
 
 const lore_items = {
     "simple_vehicles:jump1_item": [
@@ -143,24 +78,24 @@ const lore_items = {
 
 
 function updatePlayerInventoryLore() {
-  for (const player of world.getPlayers()) {
-    const inventory = player.getComponent("minecraft:inventory").container;
+    for (const player of world.getPlayers()) {
+        const inventory = player.getComponent("minecraft:inventory").container;
 
-    for (let i = 0; i < inventory.size; i++) {
-      const item = inventory.getItem(i);
+        for (let i = 0; i < inventory.size; i++) {
+            const item = inventory.getItem(i);
 
-      if (item && lore_items[item.typeId]) {
-        item.setLore(lore_items[item.typeId]);
-        inventory.setItem(i, item);
-      }
-      if (item && VEHICLES_EGGS[item.typeId]) {
-        item.setLore(VEHICLES_EGGS[item.typeId]);
-        inventory.setItem(i, item);
-      }
+            if (item && lore_items[item.typeId]) {
+                item.setLore(lore_items[item.typeId]);
+                inventory.setItem(i, item);
+            }
+            if (item && VEHICLES_EGGS[item.typeId]) {
+                item.setLore(VEHICLES_EGGS[item.typeId]);
+                inventory.setItem(i, item);
+            }
+        }
     }
-  }
 
-  system.runTimeout(updatePlayerInventoryLore, 1);
+    system.runTimeout(updatePlayerInventoryLore, 1);
 }
 
 updatePlayerInventoryLore();
